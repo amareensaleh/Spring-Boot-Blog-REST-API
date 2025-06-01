64dfff5c8898e58fb3d7b6f43cd306dc2bf4e06c
amarees <saleh.amareen@dominos.com>
2025-06-01 18:35:50 -0400

    Added a config change to lookup security user principal

--- src/main/java/com/sopromadze/blogapi/config/GraphQLifyManualConfig.java
+++ src/main/java/com/sopromadze/blogapi/config/GraphQLifyManualConfig.java
@@ -1,21 +1,49 @@
 package com.sopromadze.blogapi.config;
 
-import java.lang.reflect.Parameter;
-
 import com.graphqlify.extended.injected.InjectedParameter;
 import com.graphqlify.extended.injected.InjectedParameterMapper;
+import com.sopromadze.blogapi.security.UserPrincipal;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.lang.reflect.Parameter;
 
 @Configuration
 public class GraphQLifyManualConfig {
 
     @Bean
+    @Primary
     public InjectedParameterMapper injectedParameterMapper() {
         return new InjectedParameterMapper() {
             @Override
             protected <T> T mapInjectedParameter(Parameter parameter, InjectedParameter injectedParameter) {
-                return null;
+
+                try {
+                    Class<?> clazz = Class.forName(injectedParameter.getParameterType());
+                    if (clazz.equals(UserPrincipal.class)) {
+                        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+                        if (authentication != null) {
+                            Object principal = authentication.getPrincipal();
+                            if (principal instanceof UserPrincipal) {
+                                UserPrincipal userPrincipal = (UserPrincipal) principal;
+                                return (T) userPrincipal;
+                            }else {
+                                System.out.println("Principal is not an instance of UserPrincipal");
+                            }
+                        }else{
+                            System.out.println("Authentication object is null!");
+                        }
+                    }else {
+                        System.out.println("Unknown injected parameter type: " + injectedParameter.getParameterType());
+                    }
+                } catch (ClassNotFoundException e) {
+                    throw new RuntimeException(e);
+                }
+                throw new RuntimeException("Didn't find mapping for injected parameter: " + injectedParameter);
             }
         };
     }
